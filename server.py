4898e6a82be2b09498ae80f53de972687478a8e8
Nate Mead <nateaa4@gmail.com>
2021-08-14 16:14:14 -0500

    my portfolio website

---
+++ server.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, url_for, request, redirect, make_response
+import csv
+
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+@app.route('/<path:subpath>')
+def sub_paths(subpath='index'):
+    return render_template(subpath+'.html')
+
+def write_to_file(data):
+    with open('venv/database.txt', mode='a') as database:
+        firstname = data["firstname"]
+        lastname = data["lastname"]
+        email = data["email"]
+        message = data["message"]
+        file = database.write(f'\n{firstname},{lastname},{email},{message}')
+
+def write_to_csv(data):
+    with open('venv/database.csv', newline='', mode='a') as database2:
+        firstname = data["firstname"]
+        lastname = data["lastname"]
+        email = data["email"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([firstname,lastname,email,message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        write_to_csv(data)
+        return redirect('/thankyou')
+    else:
+        return 'something went wrong, try again'
+
+# @app.route('/generic')
+# def generic():
+#     return render_template('generic.html')
+
+# @app.route('/elements')
+# def elements():
+#     return render_template('elements.html')
+
+# @app.route('/contact')
+# def contact():
+#     return render_template('contact.html')
